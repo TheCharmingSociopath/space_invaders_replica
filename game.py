6d3f8f0eae797436a4e28694cfdf8e72ffa28b78
TheCharmingSociopath <aditya.morolia@gmail.com>
2018-05-09 20:17:22 +0530

    working, submission

--- game.py
+++ game.py
@@ -1,155 +1,124 @@
-import pygame, sys, os, time
+import pygame
+import sys
+import os
+import time
 from pygame.locals import *
 from random import randint
+from spaceship import *
+from alien import *
+from lazer import *
+from window import *
 
-clock = pygame.time.Clock()
-window = pygame.display.set_mode((800,800))
-pygame.mouse.set_visible(0)
+
 lazer_list = []
 trooper_list = []
 freeze_list = []
-
-class class_falcon:
-	def __init__(self):
-		self.falcon_img = pygame.image.load('images/falcon.png')
-		self.falcony = window.get_height() - self.falcon_img.get_height()
-		self.falconx = window.get_width() - self.falcon_img.get_width()
-
-class parent_lazers:
-	def __init__(self, falcon):
-		self.lazerx = falcon.falconx
-		self.lazery = window.get_height() - falcon.falcon_img.get_height() - self.lazer.get_height()
-	
-	def move(self):
-		self.lazery -= 10
-
-class class_lazer(parent_lazers):
-	def __init__(self, falcon):
-		self.lazer = pygame.image.load('images/lazer.png')
-		parent_lazers.__init__(self, falcon)
-
-class class_freeze(parent_lazers):
-	def __init__(self, falcon):
-		self.lazer = pygame.image.load('images/freeze.gif')
-		parent_lazers.__init__(self, falcon)
-
-class class_trooper:
-
-	def __init__(self):
-		self.trooper_img = pygame.image.load('images/trooper.png')
-		self.trooperx = randint(0, 7) * 100
-		self.troopery = randint(0, 1) * 100
-		self.spawn_time = time.time()
-		self.update_time = time.time()
-		self.dir = randint(0, 1)
-		self.frozen = False
-		self.freeze_time = -1
-		if self.dir == 1:
-			self.dir = True
-		else:
-			self.dir = False
-
-	def move(self, curr_time):
-		self.update_time = curr_time
-		if self.dir == True and self.trooperx < 700:
-			self.trooperx += 100
-			if self.trooperx >= 700:
-				self.dir = False
-		elif self.dir == False and self.trooperx > 10:
-			self.trooperx -= 100
-			if self.trooperx < 100:
-				self.dir = True
-		else:
-			self.dir = not self.dir
 
 falcon = class_falcon()
 tim = time.time()
 score = 0
 new_trooper = class_trooper()
 trooper_list.append(new_trooper)
-window.blit(new_trooper.trooper_img, (new_trooper.trooperx, new_trooper.troopery))
+window.blit(class_trooper.trooper_img, (new_trooper.x, new_trooper.y))
+
+f = open('high_score.txt', 'r+')
+high_score = int(f.readline())
+f.seek(0)
+f.truncate()
 
 while True:
-	clock.tick(60)
-	window.fill((0, 0, 0))
-	
-	curr_time = time.time()
+    clock.tick(60)
+    window.fill((0, 0, 0))
 
-	#print falcon
-	window.blit(falcon.falcon_img, (falcon.falconx, falcon.falcony))
+    curr_time = time.time()
 
-	#create troopers
-	if curr_time >= tim + 2:
-		new_trooper = class_trooper()
-		trooper_list.append(new_trooper)
-		window.blit(new_trooper.trooper_img, (new_trooper.trooperx, new_trooper.troopery))
-		tim = curr_time
-	
-	#update troopers
-	for trooper in trooper_list:
-		# destroy
-		if curr_time >= trooper.spawn_time + 8:
-			trooper_list.pop(trooper_list.index(trooper))
+    # print falcon
+    window.blit(class_falcon.falcon_img, (falcon.x, falcon.y))
 
-		for lazer in lazer_list:
-			if lazer.lazery <= trooper.troopery and lazer.lazerx == trooper.trooperx:
-				lazer_list.pop(lazer_list.index(lazer))
-				trooper_list.pop(trooper_list.index(trooper))
-				score += 1
-				break
+    # create troopers
+    if curr_time >= tim + 2:
+        new_trooper = class_trooper()
+        trooper_list.append(new_trooper)
+        window.blit(new_trooper.trooper_img, (new_trooper.x, new_trooper.y))
+        tim = curr_time
 
-		for lazer in freeze_list:
-			if lazer.lazery <= trooper.troopery and lazer.lazerx == trooper.trooperx:
-				trooper.frozen = True
-				trooper.freeze_time = curr_time
-				break
+    # update troopers
+    for trooper in trooper_list:
+        # destroy
+        if curr_time >= trooper.spawn_time + 8 and trooper_list != []:
+            trooper_list.pop(trooper_list.index(trooper))
 
-	for trooper in trooper_list:			
-		if trooper.frozen == True:
-			if trooper.freeze_time + 5 <= curr_time:
-				trooper.frozen = False
-			else:
-				window.blit(trooper.trooper_img, (trooper.trooperx, trooper.troopery))
-				continue	
-		elif curr_time - trooper.update_time >= 1:
-			trooper.move(curr_time)
-		window.blit(trooper.trooper_img, (trooper.trooperx, trooper.troopery))
+        for lazer in lazer_list:
+            if trooper_list != [] and lazer.y <= trooper.y:
+            	if lazer.x == trooper.x:
+                    lazer_list.pop(lazer_list.index(lazer))
+                    trooper_list.pop(trooper_list.index(trooper))
+                    score += 1
+                    break
 
-	#update lazers
-	for lazer in lazer_list:
-		if lazer.lazery > 0:
-			lazer.move()
-			window.blit(lazer.lazer, (lazer.lazerx, lazer.lazery))
-		else:
-			lazer_list.pop(lazer_list.index(lazer))
+        for lazer in freeze_list:
+            if lazer.y <= trooper.y and lazer.x == trooper.x:
+                trooper.frozen = True
+                trooper.freeze_time = curr_time
+                break
 
-	for lazer in freeze_list:
-		if lazer.lazery > 0:
-			lazer.move()
-			window.blit(lazer.lazer, (lazer.lazerx, lazer.lazery))
-		else:
-			freeze_list.pop(freeze_list.index(lazer))
+    for trooper in trooper_list:
+        if trooper.frozen is True:
+            if trooper.freeze_time + 5 <= curr_time:
+                trooper.frozen = False
+            else:
+                window.blit(class_trooper.tatti, (trooper.x, trooper.y))
+                continue
+        elif curr_time - trooper.update_time >= 1:
+            trooper.move(curr_time)
+        window.blit(trooper.trooper_img, (trooper.x, trooper.y))
 
-	#event loop
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-			sys.exit()
+    # update lazers
+    for lazer in lazer_list:
+        if lazer.y > 0:
+            lazer.move()
+            window.blit(lazer.lazer, (lazer.x, lazer.y))
+        elif lazer_list != []:
+            lazer_list.pop(lazer_list.index(lazer))
 
-		if event.type == pygame.KEYDOWN:
-			
-			if (event.key == pygame.K_a or event.key == pygame.K_LEFT) and falcon.falconx > 0:
-				falcon.falconx -= 100
-			
-			elif (event.key == pygame.K_d or event.key == pygame.K_RIGHT) and falcon.falconx < 700:
-				falcon.falconx += 100
-			
-			elif (event.key == pygame.K_SPACE):
-				new_lazer = class_lazer(falcon)
-				lazer_list.append(new_lazer)
-				window.blit(new_lazer.lazer, (new_lazer.lazerx, new_lazer.lazery))
-			
-			elif (event.key == pygame.K_i):
-				new_freeze = class_freeze(falcon)
-				freeze_list.append(new_freeze)
-				window.blit(new_freeze.lazer, (new_freeze.lazerx, new_freeze.lazery))
-	pygame.display.update()
+    for lazer in freeze_list:
+        if lazer.y > 0:
+            lazer.move()
+            window.blit(lazer.lazer, (lazer.x, lazer.y))
+        elif freeze_list != []:
+            freeze_list.pop(freeze_list.index(lazer))
+
+    # event loop
+    for event in pygame.event.get():
+        cond = (event.type == pygame.KEYDOWN and event.key == pygame.K_q)
+        if event.type == pygame.QUIT or cond:
+            high_score = max(high_score, score)
+            f.write(str(high_score) + '\n')
+            f.close()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                if falcon.x > 0:
+                    falcon.x -= 100
+
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                if falcon.x < 700:
+                    falcon.x += 100
+
+            elif (event.key == pygame.K_SPACE):
+                shot.play()
+                new_lazer = class_lazer(falcon)
+                lazer_list.append(new_lazer)
+                window.blit(new_lazer.lazer, (new_lazer.x, new_lazer.y))
+
+            elif (event.key == pygame.K_s):
+                shot.play()
+                new_freeze = class_freeze(falcon)
+                freeze_list.append(new_freeze)
+                window.blit(new_freeze.lazer, (new_freeze.x, new_freeze.y))
+    score_text = myfont.render("Score: " + str(score), True, white)
+    high_score_txt = myfont.render('High Score: ' + str(high_score), True, white)
+    window.blit(score_text, (0, height / 2))
+    window.blit(high_score_txt, (0, height / 2 + 30))
+    pygame.display.update()
